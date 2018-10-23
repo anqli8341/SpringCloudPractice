86a33f7800f990f19aa6d73040f0d878700cce0c
anqli <anqli@outlook.com>
2018-10-23 11:26:10 +0800

    FirstCommit

---
+++ spring-cloud-project/eureka-server/src/main/java/com/springcloud/eurekaserver/controller/QueryController.java
@@ -0,0 +1,22 @@
+package com.springcloud.eurekaserver.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author anquan li
+ */
+@RestController
+@RequestMapping("/query")
+public class QueryController {
+  @Autowired
+  EurekaClientConfigBean eurekaClientConfigBean;
+
+  @GetMapping("/eureka-server")
+  public Object getEurekaServerUrl(){
+    return eurekaClientConfigBean.getServiceUrl();
+  }
+}
